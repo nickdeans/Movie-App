0f3b01feea3cd9d2a34d1c96c0c31ed3d3f38b81
nickdeans <nickabramowicz@gmail.com>
2021-08-10 17:06:14 -0700

    added global styles component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 
+// Styles
+import { GlobalStyle } from './GlobalStyle';
+
 const App = () => {
   return (
     <div className="App">
       Start here.
+      <GlobalStyle />
     </div>
   );
 }
